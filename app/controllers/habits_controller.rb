7386e6d13fb0606aaf6d9198026edbe27ce013cb
David Sanchez <david.sanchez@envato.com>
2025-07-17 12:33:52 -0600

    Add edit habits view

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,10 +1,11 @@
 class HabitsController < ApplicationController
+  before_action :set_habit, only: [ :show, :edit, :update ]
+
   def index
     @habits = Habit.all
   end
 
   def show
-    @habit = Habit.find(params[:id])
   end
 
   def new
@@ -25,9 +26,30 @@ class HabitsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @habit.update(habit_params)
+      respond_to do |format|
+        format.html { redirect_to @habit, notice: "Habit was successfully updated." }
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def habit_params
     params.require(:habit).permit(:title, :frequency, :description)
+  end
+
+  def set_habit
+    @habit = Habit.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Habit not found."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "habits#index"
 
-  resources :habits, only: [ :index, :show, :new, :create ]
+  resources :habits
 end
